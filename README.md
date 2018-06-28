41d47b714e112d665c8023248bacfac71ec05260
Anton Bozhinov <anton.bozhinov@softwaregroup.com>
2018-06-28 14:31:47 +0300

    setup with method

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Dockerize
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/dockerize`. To experiment with that code, run `bin/console` for an interactive prompt.
+Dockerize is a ruby gem for generating applications inside docker containers.
 
-TODO: Delete this and the text above, and describe your gem
 
 ## Installation
 
@@ -22,7 +21,13 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+    $ dockerize /module/ with /db_name/
+
+## Example
+    $ dockerize rails with postgresql
+
+#### See all available options
+    $ dockerize
 
 ## Development
 
--- lib/dockerize/rails.rb
+++ lib/dockerize/rails.rb
@@ -4,15 +4,26 @@ module Dockerize
     class Rails < Thor
         desc "with <db_name>", "Creates docker-compose rails app with given database name"
         long_desc <<-LONGDESC
-        `dockerize <module> with <db_name>` will generate ruby on rails application and will connect it
+         will generate ruby on rails application and will connect it
         to a given database.
         Example:
         $ dockerize rails with postgresql
      
         > from: Anton Bozhinov
         LONGDESC
+        option :name
         def with(db_name)
-            puts "Generating rails aplication with #{db_name} as a database..."
+            if options[:name]
+                project_name = options[:name]
+            else
+                puts "What is your project name?"
+                project_name = STDIN.gets.chomp
+            end
+            puts "Generating rails aplication with #{db_name} as a database in #{project_name}..."
+
+            
+            puts "Creating #{project_name} dir in #{Dir.pwd}"
+
         end
         
     end
